8b1fd21711a64f85146f2a18afd9ba32cba61eb6
acbardivanov2007-coder <acbardivanov2007@gmail.com>
2025-11-05 22:26:11 +0700

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+int main ()
+{
+char slovo;
+int e = 0; 
+int scetchik=0;
+printf("введите преддложение с ei или # для выхода:\n");
+while ((slovo = getchar()) != '#')
+{
 
+    if (slovo == 'e')
+        e = 1;
+    else if (slovo == 'i' && e == 1)
+        {
+            scetchik++;
+            e = 0;
+        }
+    else
+        e = 0;
+}
+printf("в вашем предложении вот столько %d ei", scetchik);
+return 0;
+}
